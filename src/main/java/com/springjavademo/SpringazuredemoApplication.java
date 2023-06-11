2d01c40349a9b8d1b678ff1b6b154df11ee6fceb
Mahnaz-karimi <72239384+Mahnaz-karimi@users.noreply.github.com>
2023-06-12 00:24:07 +0200

    first commit

---
+++ src/main/java/com/springjavademo/SpringazuredemoApplication.java
@@ -0,0 +1,20 @@
+package com.springjavademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringazuredemoApplication {
+
+	@GetMapping("/message")
+	public String message(){
+		return "COngreats ! you app depolyed successfully in Azure !";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringazuredemoApplication.class, args);
+	}
+
+}
